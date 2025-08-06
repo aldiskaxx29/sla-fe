df7f21a14c3be37e3d8de4657299033d48510e54
aldiskax <muhamadaldisetiawan00@gmail.com>
2025-08-06 07:51:10 +0700

    edit e lib link aldi

--- src/modules/elibrary/pages/ELibraryPage.tsx
+++ src/modules/elibrary/pages/ELibraryPage.tsx
@@ -1,7 +1,7 @@
 const ELibraryPage = () => {
   return (
     <iframe
-      src="http://10.62.175.156/doc/umum"
+      src="https://netq.telkom.co.id/doc/umum"
       title="E Library"
       className="w-full min-h-[100vh]"
     />
