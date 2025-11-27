d11feb1f39aa02ed4a9358d1b0255778aa0cfe64
ramdhan <ramdhansetiadhi@gmail.com>
2025-11-27 16:38:42 +0700

    fix data and styling

--- src/modules/site/pages/ReportSupportNeededPage.tsx
+++ src/modules/site/pages/ReportSupportNeededPage.tsx
@@ -99,44 +99,6 @@ const ReportSupportNeededPage = () => {
       value: "12",
     },
   ];
-  const options = {
-    indexAxis: "y" as const,
-    maintainAspectRatio: false,
-    elements: {
-      bar: {
-        borderWidth: 2,
-      },
-    },
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "right" as const,
-      },
-      title: {
-        display: true,
-      },
-    },
-  };
-
-  const options3D = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {},
-    elements: {
-      bar: {
-        borderWidth: 2,
-        height: 210
-      },
-    },
-    scales: {
-      x: {
-        stacked: false,
-      },
-      y: {
-        beginAtZero: true,
-      },
-    },
-  };
   const [getCap, { data: dataCap }] = useLazyGetReportSupportUpgradeCapQuery()
   const [getNode, { data: dataNode }] = useLazyGetReportSupportUpgradeNodebQuery()
   const [getQe, { data: dataQe }] = useLazyGetReportSupportUpgradeQeQuery()
@@ -146,100 +108,71 @@ const ReportSupportNeededPage = () => {
     setLoading(true)
     const resultCap = await getCap({ query: { month: 11 } }).unwrap()
     const resultNode = await getNode({ query: { month: 11 } }).unwrap()
-   const resultQE = await getQe({ query: { month: 11 } }).unwrap()
-   const resultTsel = await getTsel({ query: { month: 11 } }).unwrap()
+    const resultQE = await getQe({ query: { month: 11 } }).unwrap()
+    const resultTsel = await getTsel({ query: { month: 11 } }).unwrap()
     setDataPie(
       {
         labels: Object.keys(resultCap.upgradeCapacity.percentage),
         datasets: [
           {
             data: Object.values(resultCap.upgradeCapacity.percentage),
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-            ],
-            borderColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-            ],
+            backgroundColor: ["#185E8B", "#EB7035", "#3B7E2D"],
+            cutout: "70%",
           },
         ],
       }
     )
     setDataBarNode(
       {
-        labels: ["Progress Status"],
+        labels: ["Done", "On Progress", "Open"],
         datasets: [
           {
-            label: "Done",
-            data: [resultNode.onePortOneNodeB.done],
-            backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-            borderColor: ["rgba(255, 99, 132, 1)"],
-          },
-          {
-            label: "On Progress",
-            data: [resultNode.onePortOneNodeB.onProgress],
-            backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-            borderColor: ["rgba(54, 162, 235, 1)"],
-          },
-          {
-            label: "Open",
-            data: [resultNode.onePortOneNodeB.open],
-            backgroundColor: ["rgba(255, 206, 86, 0.2)"],
-            borderColor: ["rgba(255, 206, 86, 1)"],
+            data: [resultNode.onePortOneNodeB.done, resultNode.onePortOneNodeB.onProgress, resultNode.onePortOneNodeB.open],
+            backgroundColor: (ctx) => {
+              const chart = ctx.chart;
+              const { ctx: g, chartArea } = chart;
+
+              if (!chartArea) return "#0e5774";
+
+              const gradient = g.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+              gradient.addColorStop(0, "#0e5774");
+              gradient.addColorStop(1, "#0f6e8e");
+              return gradient;
+            },
+            borderRadius: 8,
+            barPercentage: 0.5,
           },
         ],
       }
     )
     setDataBarQE(
       {
-        labels: ["Progress Status"],
+        labels: ["Done", "On Progress", "Open"],
         datasets: [
           {
-            label: "Done",
-            data: [resultQE.QE.done],
-            backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-            borderColor: ["rgba(255, 99, 132, 1)"],
-          },
-          {
-            label: "On Progress",
-            data: [resultQE.QE.onProgress],
-            backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-            borderColor: ["rgba(54, 162, 235, 1)"],
-          },
-          {
-            label: "Open",
-            data: [resultQE.QE.open],
-            backgroundColor: ["rgba(255, 206, 86, 0.2)"],
-            borderColor: ["rgba(255, 206, 86, 1)"],
+            label: "QE",
+            data: [resultQE.QE.done, resultQE.QE.onProgress, resultQE.QE.open],
+            backgroundColor: "#0e5774",
+            borderRadius: 6,
+            barThickness: 25,
           },
         ],
       }
     )
     setDataBarTsel(
       {
-        labels: ["Progress Status"],
+        labels: ["Done", "On Progress", "Open"],
         datasets: [
           {
-            label: "Done",
-            data: [resultTsel.TSEL.done],
-            backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-            borderColor: ["rgba(255, 99, 132, 1)"],
-          },
-          {
-            label: "On Progress",
-            data: [resultTsel.TSEL.onProgress],
-            backgroundColor: ["rgba(54, 162, 235, 0.2)"],
-            borderColor: ["rgba(54, 162, 235, 1)"],
-          },
-          {
-            label: "Open",
-            data: [resultTsel.TSEL.open],
-            backgroundColor: ["rgba(255, 206, 86, 0.2)"],
-            borderColor: ["rgba(255, 206, 86, 1)"],
-          },
+            label: "TSEL",
+            data: [resultTsel.TSEL.done, resultTsel.TSEL.onProgress, resultTsel.TSEL.open],
+            backgroundColor: "#0A4C66",        // warna utama bar
+            borderColor: "#083B4E",           // warna sisi untuk efek 3D
+            borderWidth: 10,                  // ketebalan sisi "3D"
+            borderSkipped: false,             // bikin efek kotak 3D
+            borderRadius: 2,
+
+          }
         ],
       }
     )
@@ -249,21 +182,6 @@ const ReportSupportNeededPage = () => {
   useEffect(() => {
     getData()
   }, []);
-
-  const total = 8700;
-  const dataValues = [34, 52, 14]; // persen
-
-  const dataPieD = {
-    labels: ["Done", "On Progress", "Open"],
-    datasets: [
-      {
-        data: dataValues,
-        backgroundColor: ["#185E8B", "#EB7035", "#3B7E2D"],
-        borderWidth: 0,
-        cutout: "70%", //  donut
-      },
-    ],
-  };
 
   const optionsD = {
     plugins: {
@@ -279,29 +197,6 @@ const ReportSupportNeededPage = () => {
         formatter: (value) => `${value}%`,
       },
     },
-  };
-
-  const dataBarNodeB = {
-    labels: ["Done", "On Progress", "Open"],
-    datasets: [
-      {
-        label: "",
-        data: [3000, 4500, 1200],
-        backgroundColor: (ctx) => {
-          const chart = ctx.chart;
-          const { ctx: g, chartArea } = chart;
-  
-          if (!chartArea) return "#0e5774";
-  
-          const gradient = g.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
-          gradient.addColorStop(0, "#0e5774");
-          gradient.addColorStop(1, "#0f6e8e");
-          return gradient;
-        },
-        borderRadius: 8,
-        barPercentage: 0.5,
-      },
-    ],
   };
 
   const options3DB = {
@@ -335,40 +230,6 @@ const ReportSupportNeededPage = () => {
         shadowColor: "rgba(0,0,0,0.25)",
       },
     },
-  };
-  
-  // ChartJS.register({
-  //   id: "barShadow",
-  //   beforeDraw: (chart) => {
-  //     const ctx = chart.ctx;
-  //     chart.data.datasets.forEach((dataset, i) => {
-  //       const meta = chart.getDatasetMeta(i);
-  
-  //       meta.data.forEach((bar) => {
-  //         ctx.save();
-  //         ctx.shadowColor = "rgba(0,0,0,0.25)";
-  //         ctx.shadowBlur = 20;
-  //         ctx.shadowOffsetX = 4;
-  //         ctx.shadowOffsetY = 12;
-  //         ctx.fillStyle = bar.options.backgroundColor;
-  //         ctx.fillRect(bar.x - bar.width / 2, bar.y, bar.width, bar.base - bar.y);
-  //         ctx.restore();
-  //       });
-  //     });
-  //   },
-  // });
-
-  const dataBarQEB = {
-    labels: ["Done", "On Progress", "Open"],
-    datasets: [
-      {
-        label: "",
-        data: [3000, 4500, 1200],
-        backgroundColor: "#0e5774",
-        borderRadius: 6,
-        barThickness: 25,
-      },
-    ],
   };
 
   const optionsB = {
@@ -408,35 +269,30 @@ const ReportSupportNeededPage = () => {
     },
   };
 
-  const dataBarTselB = {
-    labels: ["Done", "On Progress", "Open"],
-    datasets: [
-      {
-        label: "TSEL",
-        data: [3000, 4500, 1200],
-        backgroundColor: "#0A4C66",        // warna utama bar
-        borderColor: "#083B4E",           // warna sisi untuk efek 3D
-        borderWidth: 10,                  // ketebalan sisi "3D"
-        borderSkipped: false,             // bikin efek kotak 3D
-        borderRadius: 2,
-      }
-    ]
-  };
-
   const optionsTselB = {
     responsive: true,
     maintainAspectRatio: false,
     layout: { padding: 0 },
     plugins: {
-      legend: { 
+      legend: {
         display: false,
         labels: {
           color: "#ffffff"
-        }
+        },
       },
       tooltip: {
         titleColor: "#ffffff",
         bodyColor: "#ffffff"
+      },
+      datalabels: {
+        color: "#ffffff",  // warna text di dalam bar
+        anchor: "center",
+        align: "center",
+        font: {
+          weight: "bold",
+          size: 14
+        },
+        formatter: (value) => value
       }
     },
     scales: {
@@ -445,7 +301,7 @@ const ReportSupportNeededPage = () => {
         offset: false,
         grace: 0,
         ticks: {
-          color: "#ffffff"   // ← warna teks sumbu Y
+          color: "#000000"   // ← warna teks sumbu Y
         },
         grid: {
           drawBorder: false
@@ -453,14 +309,11 @@ const ReportSupportNeededPage = () => {
       },
       x: {
         ticks: {
-          color: "#ffffff"   // ← warna teks sumbu X
+          color: "#000000"   // ← warna teks sumbu X
         }
       }
     }
   };
-  
-  
-  
 
   return (
     <div className="bg-white border border-[#DBDBDB] rounded-xl p-4 m-6">
@@ -481,34 +334,53 @@ const ReportSupportNeededPage = () => {
       <section className="grid grid-cols-4 gap-4 mt-4">
         <div className="p-6 bg-neutral-100 rounded-2xl shadow-sm">
           <h2 className="text-lg font-semibold">Upgrade Capacity</h2>
-
-          {/* <Pie data={dataPie} /> */}
-          <Doughnut data={dataPieD} options={optionsD} />
+          <Doughnut data={dataPie} options={optionsD} />
+          <p className="text-lg font-semibold text-center">Total: {dataCap && dataCap?.upgradeCapacity?.totalSite}</p>
           <TableDetailReportSupport data={dataCap?.data[0].data} />
         </div>
         <div className="p-6 bg-neutral-100 rounded-2xl shadow-sm">
           <h2 className="text-lg font-semibold">1 Port 1 Node B</h2>
           <div className="w-full aspect-square">
-
-          {/* <Bar data={dataBarNode} options={options3D} /> */}
-          <Bar data={dataBarNodeB} options={options3DB} />
+            <Bar data={dataBarNode} options={options3DB} />
           </div>
+          <p className="text-lg font-semibold text-center">Total: {
+            (() => {
+              if (!dataNode) return 0;
+              const values = Object.values((dataNode as any)?.onePortOneNodeB ?? {});
+              const nums = values.map((v) => Number(v ?? 0));
+              return nums.reduce((sum, val) => sum + val, 0);
+            })()
+          }</p>
           <TableDetailReportSupport data={dataNode?.data[0].data} />
         </div>
         <div className="p-6 bg-neutral-100 rounded-2xl shadow-sm">
           <h2 className="text-lg font-semibold">QE</h2>
           <div className="w-full aspect-square">
-
-          <Bar data={dataBarQEB} options={optionsB} />
+            <Bar data={dataBarQE} options={optionsB} />
           </div>
+          <p className="text-lg font-semibold text-center">Total: {
+            (() => {
+              if (!dataQe) return 0;
+              const values = Object.values((dataQe as any)?.QE ?? {});
+              const nums = values.map((v) => Number(v ?? 0));
+              return nums.reduce((sum, val) => sum + val, 0);
+            })()
+          }</p>
           <TableDetailReportSupport data={dataQe?.data[0].data} />
         </div>
         <div className="p-6 bg-neutral-100 rounded-2xl shadow-sm">
           <h2 className="text-lg font-semibold">TSEL</h2>
           <div className="w-full aspect-square">
-
-          <Bar data={dataBarTselB} options={optionsTselB} />
+            <Bar data={dataBarTsel} options={optionsTselB} />
           </div>
+          <p className="text-lg font-semibold text-center">Total: {
+            (() => {
+              if (!dataTsel) return 0;
+              const values = Object.values((dataTsel as any)?.TSEL ?? {});
+              const nums = values.map((v) => Number(v ?? 0));
+              return nums.reduce((sum, val) => sum + val, 0);
+            })()
+          }</p>
           <TableDetailReportSupport data={dataTsel?.data[0].data} />
         </div>
       </section>
